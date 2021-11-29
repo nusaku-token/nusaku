56cdaf502f3b931b61231e8a139281bc575591c4
NUSAKU <94667632+nusaku-token@users.noreply.github.com>
2021-11-29 10:29:35 +0700

    Update cta.js
    Change Typo

--- components/cta.js
+++ components/cta.js
@@ -8,7 +8,7 @@ export default function Cta() {
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">You want to participate to buy nusaku in private sale</h2>
           <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-            Contact our marketing on privatesale@nusaku.id or field the Private sale form...
+            Contact our marketing on privatesale@nusaku.id or fill the Private sale form...
           </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
